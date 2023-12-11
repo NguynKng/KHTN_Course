9e30dcaa7f60034b8d547094855cd0de5b3ff7ef
NguynKng <harrybin2109@gmail.com>
2023-12-11 20:54:23 +0700

    Bai tap tuan 2

--- Week2/bai13.py
+++ Week2/bai13.py
@@ -5,3 +5,4 @@ new_list3 = [x if x>0 else 0 for x in or_lst]
 print("List mới sau khi mỗi phần tử tăng thêm 2:",new_list1)
 print("List sau khi lọc phan tu am:",new_list2)
 print("List thay thế các phần tử âm thành số 0:",new_list3)
+#Test git
